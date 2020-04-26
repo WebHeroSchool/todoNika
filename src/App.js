bdf8e9dabf971a6d7b19b5d2e89c6d6d6ff4918b
nikakrash <nikakrash@ya.ru>
2020-04-26 13:49:15 +0500

    added examples about jsx

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const salmon = '#fa8072';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header" style={{backgroundColor: 'salmon'}}>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <h1>
+          Hey you!
+        </h1>
+        <div>
+          I used for background color {salmon}
+        </div>
+        <div>
+          {42}
+        </div>
+        <div>
+          {41 + 1}
+        </div>
+        <div>
+          {42 && '42'}
+        </div>
+        <div>
+          {42 ? 42 : '42'}
+        </div>
+        <div>
+          {undefined}
+          {null}
+          {false}
+          {true}
+        </div>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
