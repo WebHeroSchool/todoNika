d750a6de2ab78fc1f25bd0bc0aa3209658fdce32
nikakrash <nikakrash@ya.ru>
2020-05-23 13:58:13 +0500

    added event checker

--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -2,11 +2,11 @@ import React from 'react';
 import Item from '../Item/Item';
 import styles from './ItemList.module.css';
 
-const ItemList = ({ items }) => (
+const ItemList = ({ items, onClickDone }) => (
 	<div className={styles.list}>
 		{items.map(item => 
 			<div key={item.value}>
-				<Item value = {item.value} isDone={item.isDone}/>
+				<Item value = {item.value} isDone={item.isDone} onClickDone={onClickDone}/>
 			</div> )
 		}
 	</div>
