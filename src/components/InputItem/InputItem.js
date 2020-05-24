4e17f2effc7abca9d97b71727b8496f510653300
nikakrash <nikakrash@ya.ru>
2020-05-24 21:26:47 +0500

    added prop-types

--- src/components/InputItem/InputItem.js
+++ src/components/InputItem/InputItem.js
@@ -2,6 +2,7 @@ import React from 'react';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import styles from './InputItem.module.css';
+import PropTypes from 'prop-types';
 
 class InputItem extends React.Component {
 	state = {
@@ -46,5 +47,9 @@ class InputItem extends React.Component {
 		);
 	}
 }
+
+InputItem.propTypes = {
+	inputValue: PropTypes.string
+};
 
 export default InputItem;
