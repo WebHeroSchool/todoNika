e50a955216bed13ed245d85176bdefaac5037ec9
nikakrash <nikakrash@ya.ru>
2020-05-28 11:03:45 +0500

    added requests

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,12 +1,41 @@
 import React from 'react';
+import { Octokit } from '@octokit/rest';
 import styles from './About.module.css';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
-export default class About extends React.Component {
+const octokit = new Octokit();
+
+class About extends React.Component {
+	state = {
+		isLoading: true,
+		repoList: []
+	}
+
+	conponentDidMount() {
+		octokit.repos.listForUser({
+			username: '6thSence'
+		}).then(({ data }) => {
+			this.setState({
+				repoList: data,
+				isLoading: false
+			});
+		});
+	}
+
     render () {
+    	const { isLoading, repoList } = this.state;
         return (
             <div className={styles.wrap}>
-                Привет! Я Ника :)
+            	{ isLoading ?  <CircularProgress /> : " Привет! Я Ника :) Вот мои репозитории:" }
+            	{ !isLoading && <ul>
+            			{repoList.map(repo => (<li>
+            					{repo.name}
+            				</li>
+            				) )}
+            		</ul>}
             </div>
         )
     }
-}
+};
+
+export default About;
