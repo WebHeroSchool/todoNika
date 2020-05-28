5cab3f93b8c4afd5c1546c66d1f906e39e3975c6
nikakrash <nikakrash@ya.ru>
2020-05-28 18:40:59 +0500

    added requests

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -8,28 +8,68 @@ const octokit = new Octokit();
 class About extends React.Component {
 	state = {
 		isLoading: true,
-		repoList: []
-	}
+		repoList: [],
+        isError: false,
+        errorMessage: '',
+        userInfo: [],
+        user: 'nikakrash'
+	};
 
-	conponentDidMount() {
+	componentDidMount() {
+        const { user } = this.state;
+
 		octokit.repos.listForUser({
-			username: '6thSence'
+			username: user
 		}).then(({ data }) => {
 			this.setState({
-				repoList: data,
-				isLoading: false
-			});
-		});
+                repoList: data,
+                isLoading: false
+            });
+		})
+
+        .catch(err => {
+            this.setState({
+                isError: true,
+                errorMessage: 'Кажется, такого репоитория не существует',
+                isLoading: false
+            })
+        });
+
+        octokit.users.getByUsername({
+            username: user
+        }).then(({ data }) => {console.log(data);
+            this.setState({
+                userInfo: data,
+                isLoading: false
+            });
+        })
+
+        .catch(err => {
+            this.setState({
+                isLoading: false,
+                isError: true,
+                errorMessage: 'Кажется, такого репоитория не существует'
+            });
+        });
 	}
 
     render () {
-    	const { isLoading, repoList } = this.state;
+    	const { isLoading, repoList, isError, errorMessage, userInfo} = this.state;
         return (
             <div className={styles.wrap}>
-            	{ isLoading ?  <CircularProgress /> : " Привет! Я Ника :) Вот мои репозитории:" }
+            	{ isLoading ?  <CircularProgress /> : <div>Привет!</div> }
+
+                <div className={styles.info}>
+                    {!isError ? <div> Я {userInfo.bio}, мой логин на гитхабе {userInfo.login}</div> : '' }
+                </div>
+                
+                {isError ? <div className={styles.error}>Ошибка: { errorMessage } </div> : 'А вот мои репозитории:'} 
+
             	{ !isLoading && <ul>
             			{repoList.map(repo => (<li>
-            					{repo.name}
+            					<a href={repo.html_url} className={styles.link} target="_blank">
+                                    {repo.name}
+                                </a>
             				</li>
             				) )}
             		</ul>}
